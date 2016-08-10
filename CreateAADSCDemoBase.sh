66ed1af3e37f2e471958336dddee9eb39c059d05
Kevin Remde <kevin@remde.net>
2016-08-10 11:32:07 -0500

    Added xplat-cli script version

---
+++ CreateAADSCDemoBase.sh
@@ -0,0 +1,28 @@
+﻿#!/bin/bash
+
+# Set globals here before running the script
+# Important: StorageAccountName should be lowercase
+
+PublishSettingsFileLocation="/home/Kevin/AzureSub.publishsettings"
+SubscriptionName="KevRem Azure"
+rgName="rgtest"
+assetLocation="https://raw.githubusercontent.com/KevinRemde/AADSCDemoBase/master/"
+templateFileURI=$assetLocation"azuredeploy.json"
+templateFilePath="./azuredeploy.json"
+parmFilePath="./azuredeploy.parameters.json"
+
+# Add Azure account
+azure account import $PublishSettingsFileLocation
+
+# Set the Azure account you want to use
+azure account set "$SubscriptionName"
+
+# Set ARM mode
+azure config mode ARM
+
+# Create the Resource Group
+azure group create -n $rgName -l "West US"
+
+# Deploy!
+#azure group deployment create $rgName -n testRGDeploy -e $parmFilePath -f $templateFilePath  
+azure group deployment create $rgName -n testRGDeploy -e $parmFilePath --template-uri $templateFileURI 
